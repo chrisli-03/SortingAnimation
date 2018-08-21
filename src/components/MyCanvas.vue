952d129ec07b78d000cfec4d1da04d02361af79f
Hecheng Li <lihecheng93@gmail.com>
2018-08-21 20:52:50 +0800

    Functional random swap

--- src/components/MyCanvas.vue
+++ src/components/MyCanvas.vue
@@ -1,15 +1,15 @@
 <template>
 	<svg id="my-svg" v-bind:width="panelWidth" v-bind:height="panelHeight">
 		<g v-for="(item, index) in data" class="bar" shape-rendering="crispEdges"
-			v-bind:class="{ selected: index === -1 }">
+			v-bind:class="{ hidden: index === m || index === n }">
 			<rect v-bind="{ 'x':index*barWidth, 'y':panelHeight-item*heightRatio }" v-bind:width="barWidth" v-bind:height="item*heightRatio"/>
-    </g>
-		<g class="bar selected" shape-rendering="crispEdges">
-			<rect v-bind="{ 'x':m*barWidth, 'y':panelHeight-data[m]*heightRatio }" v-bind:width="barWidth" v-bind:height="data[m]*heightRatio"/>
-    </g>
-		<g class="bar selected" shape-rendering="crispEdges">
-			<rect v-bind="{ 'x':n*barWidth, 'y':panelHeight-data[n]*heightRatio }" v-bind:width="barWidth" v-bind:height="data[n]*heightRatio"/>
-    </g>
+    	</g>
+		<g v-if="m > 0" class="bar selected" shape-rendering="crispEdges">
+			<rect v-bind="{ 'x':(m+diff)*barWidth, 'y':panelHeight-data[m]*heightRatio }" v-bind:width="barWidth" v-bind:height="data[m]*heightRatio"/>
+    	</g>
+		<g v-if="n > 0" class="bar selected" shape-rendering="crispEdges">
+			<rect v-bind="{ 'x':(n-diff)*barWidth, 'y':panelHeight-data[n]*heightRatio }" v-bind:width="barWidth" v-bind:height="data[n]*heightRatio"/>
+    	</g>
 	</svg>
 </template>
 
@@ -21,8 +21,9 @@ export default {
 			panelWidth: 1,
 			panelHeight: 1,
 			maxHeight: 1,
-			m: 3,
-			n: 20
+			m: -1,
+			n: -1,
+			diff: 0
 		}
 	},
 	methods: {
@@ -44,10 +45,30 @@ export default {
 			}
 			this.data = array
 		},
-		swap() {
-			let tempM = this.m
-			let tempN = this.n
-
+		swap(m, n) {
+			this.m = Math.min(m, n)
+			this.n = Math.max(m, n)
+			let _this = this
+			let promise = new Promise((resolve) => {
+				let done = Math.abs(_this.m - _this.n)
+				let interval = setInterval(() => {
+					if (_this.diff < done) _this.diff += 0.5
+					else {
+						[_this.data[_this.m], _this.data[_this.n]] = [_this.data[_this.n], _this.data[_this.m]]
+						_this.m = -1
+						_this.n = -1
+						_this.diff = 0
+						clearInterval(interval)
+						resolve(0)
+					}
+				}, 1000/60)
+			})
+			promise.then((val) => {
+				let length = this.data.length
+				let m = Math.floor(Math.random()*length)
+				let n = Math.floor(Math.random()*length)
+				this.swap(m, n)
+			})
 		}
 	},
 	computed: {
@@ -58,9 +79,16 @@ export default {
 			return this.panelHeight/this.maxHeight
 		}
 	},
-	mounted: function() {
+	mounted: async function() {
+		/* let newData = this.data
+		for (let i = 0; i < 10000; i++) {
+			newData.push(i)
+		}
+		this.data = newData
+		this.shuffle() */
 		window.addEventListener('resize', this.onResize)
 		this.onResize()
+		this.swap(0, 2)
 	},
 	beforeDestroy: function() {
 		window.removeEventListener('resize', this.onResize)
@@ -71,4 +99,5 @@ export default {
 <style scoped>
 	.bar { fill: gold }
 	.selected { fill: lightskyblue }
+	.hidden { fill: transparent }
 </style>
