7557105d309c0dada75a166b1210ef88fabc2102
Hecheng(Chris) Li <lihecheng93@gmail.com>
2018-08-22 17:05:28 +0800

    Modified Built Files

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'SAGraph/dist/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
