7fee4d7b465369a4e1d363609a9925fe1d15806c
Hecheng(Chris) Li <lihecheng93@gmail.com>
2018-08-23 11:29:34 +0800

    Removed Outdated Shuffle Function In DataPanel

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -10,18 +10,7 @@ export default {
 		}
 	},
 	methods: {
-		shuffle() {
-			let array = this.data
-			let currentIndex = array.length, temporaryValue, randomIndex;
-			while (0 !== currentIndex) {
-				randomIndex = Math.floor(Math.random() * currentIndex);
-				currentIndex -= 1;
-				temporaryValue = array[currentIndex];
-				array[currentIndex] = array[randomIndex];
-				array[randomIndex] = temporaryValue;
-			}
-			this.data = array
-		}
+		
 	}
 }
 </script>
